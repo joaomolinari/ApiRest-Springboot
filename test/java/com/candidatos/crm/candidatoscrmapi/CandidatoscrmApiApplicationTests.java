353d9ba9b8ddda1996bb067fa58759811107d04f
João Vitor Aguilar Molinari <92277701+joaomolinari@users.noreply.github.com>
2022-11-16 14:34:12 -0300

    Add files via upload

---
+++ test/java/com/candidatos/crm/candidatoscrmapi/CandidatoscrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candidatos.crm.candidatoscrmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatoscrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
